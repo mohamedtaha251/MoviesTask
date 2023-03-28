ef8d7679711e750186beb0b8e0cbd41d8887ed3c
mohamed.taha <mohammed.taha@shgardi.app>
2023-03-28 23:53:57 +0200

    add paging source

---
+++ app/src/main/java/mohamed/taha/moviestask/paging/NowPlayingFilmSource.kt
@@ -0,0 +1,34 @@
+package mohamed.taha.moviestask.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import mohamed.taha.moviestask.model.Film
+import mohamed.taha.moviestask.remote.ApiService
+import mohamed.taha.moviestask.util.FilmType
+
+import retrofit2.HttpException
+import java.io.IOException
+
+class NowPlayingFilmSource(private val api: ApiService, private val filmType: FilmType) :
+    PagingSource<Int, Film>() {
+    override fun getRefreshKey(state: PagingState<Int, Film>): Int? = state.anchorPosition
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Film> {
+        return try {
+            val nextPage = params.key ?: 1
+            val nowPlayingMovies =
+                if (filmType == FilmType.MOVIE) api.getNowPlayingMovies(page = nextPage)
+                else api.getOnTheAirTvShows(page = nextPage)
+
+            LoadResult.Page(
+                data = nowPlayingMovies.results,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (nowPlayingMovies.results.isEmpty()) null else nowPlayingMovies.page + 1
+            )
+        } catch (e: IOException) {
+            return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            return LoadResult.Error(e)
+        }
+    }
+}
---
+++ app/src/main/java/mohamed/taha/moviestask/paging/SearchFilmSource.kt
@@ -0,0 +1,36 @@
+package mohamed.taha.moviestask.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import mohamed.taha.moviestask.model.Search
+import mohamed.taha.moviestask.remote.ApiService
+import retrofit2.HttpException
+import java.io.IOException
+
+class SearchFilmSource(
+    private val api: ApiService,
+    private val searchParams: String,
+    private val includeAdult: Boolean
+) : PagingSource<Int, Search>() {
+    override fun getRefreshKey(state: PagingState<Int, Search>): Int? = state.anchorPosition
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Search> {
+        return try {
+            val nextPage = params.key ?: 1
+            val searchMovies = api.multiSearch(
+                page = nextPage,
+                searchParams = searchParams,
+                includeAdult = includeAdult
+            )
+            LoadResult.Page(
+                data = searchMovies.results,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (searchMovies.results.isEmpty()) null else searchMovies.page + 1
+            )
+        } catch (e: IOException) {
+            return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            return LoadResult.Error(e)
+        }
+    }
+}
