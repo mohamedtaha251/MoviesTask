db3b7e77817e92031cd7e6d3084626da884970f1
mohamed.taha <mohammed.taha@shgardi.app>
2023-03-28 23:47:42 +0200

    add depedency injection using hilt

---
+++ app/src/main/java/mohamed/taha/moviestask/di/AppModule.kt
@@ -0,0 +1,84 @@
+package mohamed.taha.moviestask.di
+
+import android.app.Application
+import androidx.room.Room
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import mohamed.taha.moviestask.local.WatchListDatabase
+import mohamed.taha.moviestask.remote.ApiService
+import mohamed.taha.moviestask.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesAPIService(okHttpClient: OkHttpClient): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(ApiService::class.java)
+    }
+
+//    @Singleton
+//    @Provides
+//    fun provideMoviesRepository(api: ApiService) = FilmRepository(api = api)
+//
+//    @Singleton
+//    @Provides
+//    fun provideSearchRepository(api: ApiService) = SearchRepository(api = api)
+//
+//    @Singleton
+//    @Provides
+//    fun providesGenresRepository(api: ApiService) = GenreRepository(api)
+//
+//    @Singleton
+//    @Provides
+//    fun providesWatchListRepository(watchListDatabase: WatchListDatabase) =
+//        WatchListRepository(database = watchListDatabase)
+//
+//    @Singleton
+//    @Provides
+//    fun providesReviewsRepository(api: ApiService): ReviewsRepository = ReviewsRepository(api)
+
+    @Provides
+    @Singleton
+    fun providesWatchListDatabase(application: Application): WatchListDatabase {
+        return Room.databaseBuilder(
+            application.applicationContext,
+            WatchListDatabase::class.java,
+            "watch_list_db"
+        ).fallbackToDestructiveMigration().build()
+    }
+
+
+}
