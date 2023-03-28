445c6bd05caf7011e184c7807cfbc40a2d200a53
mohamed.taha <mohammed.taha@shgardi.app>
2023-03-29 01:11:57 +0200

    add shared composable

---
+++ app/src/main/java/mohamed/taha/moviestask/ui/sharedComposables/BackButton.kt
@@ -0,0 +1,27 @@
+package mohamed.taha.moviestask.ui.sharedComposables
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import mohamed.taha.moviestask.ui.theme.AppOnPrimaryColor
+import mohamed.taha.moviestask.ui.theme.ButtonColor
+
+
+@Composable
+fun BackButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    FloatingActionButton(
+        modifier = modifier.size(42.dp),
+        backgroundColor = ButtonColor,
+        contentColor = AppOnPrimaryColor,
+        onClick = { onClick() }) {
+        Icon(
+            imageVector = Icons.Rounded.ArrowBack,
+            contentDescription = "back icon",
+        )
+    }
+}
